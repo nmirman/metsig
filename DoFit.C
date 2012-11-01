beaf2cf9c052bd60c9cce4909fc5d0e138b42c8b
nmirman <nem46@cornell.edu>
2012-11-01 11:37:58 -0500

    Added comments.

--- DoFit.C
+++ DoFit.C
@@ -2,9 +2,11 @@
 
 int main(){
 
+   // declarations
    Fitter fitter;
    std::vector<event> eventvec_MC;
 
+   // fill eventvec, run minimizer
    fitter.ReadNtuple( "/eos/uscms/store/user/nmirman/Zmumu/Zmumu_ntuple_20121005.root",
          eventvec_MC, true);
    fitter.MatchMCjets( eventvec_MC );
